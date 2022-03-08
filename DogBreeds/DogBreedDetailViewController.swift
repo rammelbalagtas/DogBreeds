5d09aa754915dfc522d128143e2975affea9c234
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-07 19:43:35 -0500

    Create detail page
    Create a detail page which will display a random Dog image when navigating from the table view

---
+++ DogBreeds/DogBreedDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DogBreedDetailViewController.swift
+//  DogBreeds
+//
+//  Created by Rammel on 2022-03-07.
+//
+
+import UIKit
+
+class DogBreedDetailViewController: UIViewController {
+    
+    var dogBreed: DogBreed!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
